c7f6d3ca3187700342b36936bce2975b350084ac
dc <eopoxf@gmail.com>
2024-05-07 21:18:22 +0530

    add required migrations

--- api/models/ResourceMetadata.py
+++ api/models/ResourceMetadata.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-from api.models import Dataset, Metadata
+from api.models import Metadata, Resource
 
 
 class ResourceMetadata(models.Model):
-    resource = models.ForeignKey(Dataset, on_delete=models.CASCADE, null=False, blank=False)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, null=False, blank=False)
     metadata_item = models.ForeignKey(Metadata, on_delete=models.CASCADE, null=False, blank=False)
     value = models.CharField(max_length=1000, unique=False)
