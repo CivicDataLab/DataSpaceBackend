60616c08a8adcc0e55c8b53927c87df0d8fe91ac
dc <eopoxf@gmail.com>
2024-05-10 18:55:53 +0530

    change naming of resources under access model

--- api/types/type_access_model.py
+++ api/types/type_access_model.py
@@ -11,9 +11,9 @@ class TypeAccessModelResource:
 
 @strawberry_django.type(AccessModel, fields="__all__")
 class TypeAccessModel:
-    resources: list[TypeAccessModelResource]
+    model_resources: list[TypeAccessModelResource]
 
-    def resources(self, info) -> list[TypeAccessModelResource]:
+    def model_resources(self, info) -> list[TypeAccessModelResource]:
         try:
             return AccessModelResource.objects.filter(access_model=self.id)
         except AccessModelResource.DoesNotExist as e:
