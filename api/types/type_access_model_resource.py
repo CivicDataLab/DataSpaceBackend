1615cc3e383e0ba07a53bf89ca5e8921c6abab32
dc <eopoxf@gmail.com>
2024-05-16 15:48:42 +0530

    send fields as part of access model

--- api/types/type_access_model_resource.py
+++ api/types/type_access_model_resource.py
@@ -2,8 +2,10 @@ import strawberry_django
 
 from api.models import AccessModelResource
 from api.types import TypeResource
+from api.types.type_resource import TypeResourceSchema
 
 
 @strawberry_django.type(AccessModelResource, fields="__all__")
 class TypeAccessModelResource:
     resource: TypeResource
+    fields: TypeResourceSchema
