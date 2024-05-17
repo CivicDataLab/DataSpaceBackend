9b72cf45efd613c238f8e81259252b7c164d2fa0
dc <eopoxf@gmail.com>
2024-05-17 13:44:22 +0530

    add metadata filters

--- api/types/type_metadata.py
+++ api/types/type_metadata.py
@@ -3,6 +3,12 @@ import strawberry_django
 from api.models import Metadata
 
 
-@strawberry_django.type(Metadata, fields="__all__")
+@strawberry_django.filter(Metadata)
+class MetadataFilter:
+    model: str
+    enabled: bool
+
+
+@strawberry_django.type(Metadata, fields="__all__", filters=MetadataFilter)
 class TypeMetadata:
     pass
