f12da24fbaafd66b40d6eefe7a051e36ef1f120e
dc <eopoxf@gmail.com>
2024-09-17 19:08:06 +0530

    add download chart endpoint

--- api/views/download.py
+++ api/views/download.py
@@ -3,16 +3,35 @@ import os
 import magic
 from django.http import HttpResponse
 
-from api.models import Resource
+from api.models import Resource, ResourceChartDetails
+from api.types.type_resource_chart import chart_base
+from pyecharts_snapshot.main import make_a_snapshot
 
 
 def download(request, type, id):
-    resource = Resource.objects.get(pk=id)
-    file_path = resource.resourcefiledetails.file.name
-    if len(file_path):
-        mime_type = magic.from_buffer(resource.resourcefiledetails.file.read(), mime=True)
-        response = HttpResponse(resource.resourcefiledetails.file, content_type=mime_type)
-        response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
-    else:
-        response = HttpResponse("file doesnt exist", content_type='text/plain')
-    return response
+    if type == "resource":
+        resource = Resource.objects.get(pk=id)
+        file_path = resource.resourcefiledetails.file.name
+        if len(file_path):
+            mime_type = magic.from_buffer(resource.resourcefiledetails.file.read(), mime=True)
+            response = HttpResponse(resource.resourcefiledetails.file, content_type=mime_type)
+            response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
+        else:
+            response = HttpResponse("file doesnt exist", content_type='text/plain')
+        return response
+    elif type == "chart":
+        resource_chart = ResourceChartDetails.objects.get(pk=id)
+        response = await generate_chart(resource_chart)
+        response['Content-Disposition'] = 'attachment; filename="chart.png"'
+        return response
+
+
+async def generate_chart(resource_chart:ResourceChartDetails):
+    chart_ = chart_base(resource_chart)
+    chart_.render("snapshot.html")
+    image_file_name = "snapshot.png"
+    await make_a_snapshot("snapshot.html", image_file_name)
+    # await make_snapshot(snapshot, "snapshot.html", image_file_name)
+    with open(image_file_name, "rb") as f:
+        image_data = f.read()
+    return HttpResponse(image_data, content_type="image/png")
