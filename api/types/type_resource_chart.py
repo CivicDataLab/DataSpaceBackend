af0981986ea66e3ccb25478c152bb5752ac9be97
dc <eopoxf@gmail.com>
2024-09-27 18:26:29 +0530

    lower case the type check

--- api/types/type_resource_chart.py
+++ api/types/type_resource_chart.py
@@ -28,7 +28,7 @@ CHART_TYPE_MAP = {
 
 
 def chart_base(chart_details: ResourceChartDetails) -> Optional[Chart]:
-    if chart_details.resource.resourcefiledetails.format != "csv":
+    if chart_details.resource.resourcefiledetails.format.lower() != "csv":
         return None
     # Load the data
     data = pd.read_csv(chart_details.resource.resourcefiledetails.file.path)
