147e679f112ff2b1415f08ede22601f201408f89
dc <eopoxf@gmail.com>
2024-10-29 12:07:45 +0530

    add query to give chart images by datasetid

--- api/schema/resoure_chart_image_schema.py
+++ api/schema/resoure_chart_image_schema.py
@@ -23,6 +23,12 @@ class ResourceChartImageInputPartial:
 class Query:
     resource_chart_images: list[TypeResourceChartImage] = strawberry_django.field()
 
+    @strawberry.type(name="Query")
+    class Query:
+        @strawberry_django.field
+        def dataset_resource_charts(self, info, dataset_id: uuid.UUID) -> list[TypeResourceChartImage]:
+            return ResourceChartImage.objects.filter(dataset_id=dataset_id)
+
 
 @strawberry.type
 class Mutation:
