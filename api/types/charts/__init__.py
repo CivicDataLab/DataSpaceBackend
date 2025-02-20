da5458a43eda4b033b06456bafb8d1acdc5ab895
dc <eopoxf@gmail.com>
2025-02-20 14:45:53 +0530

    add multiline chart to charts package

--- api/types/charts/__init__.py
+++ api/types/charts/__init__.py
@@ -3,3 +3,4 @@ from api.types.charts.map_chart import MapChart
 from api.types.charts.grouped_bar_chart import GroupedBarChart
 from api.types.charts.map_chart import AssamRCChart
 from api.types.charts.map_chart import AssamDistrictChart
+from api.types.charts.multiline_chart import MultiLineChart
