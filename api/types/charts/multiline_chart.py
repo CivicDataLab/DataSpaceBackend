0b290a8c1c27c61096695091d90ddbba6dafb4ad
dc <eopoxf@gmail.com>
2025-02-27 12:17:48 +0530

    use common options for multiline

--- api/types/charts/multiline_chart.py
+++ api/types/charts/multiline_chart.py
@@ -91,78 +91,10 @@ class MultiLineChart(BaseChart):
                     )
                 )
 
-    def initialize_chart(self, filtered_data: pd.DataFrame) -> Chart:
-        """Initialize a new line chart instance with basic options."""
-        self.filtered_data = filtered_data
-        
-        chart = self.get_chart_class()(
-            init_opts=opts.InitOpts(
-                width=self.options.get('width', '100%'),
-                height=self.options.get('height', '400px'),
-                animation_opts=opts.AnimationOpts(animation=False)
-            )
+    def get_init_opts(self) -> opts.InitOpts:
+        """Override to provide line chart specific initialization options."""
+        return opts.InitOpts(
+            width=self.options.get('width', '100%'),
+            height=self.options.get('height', '400px'),
+            theme=self.options.get('theme', 'white')
         )
-
-        # Set global options
-        chart.set_global_opts(
-            title_opts=opts.TitleOpts(pos_top="5%"),  # Title 5% from top
-            legend_opts=opts.LegendOpts(
-                pos_top="5%",  # Legend 5% from top
-                pos_left="center",  # Center horizontally
-                padding=[0, 10, 20, 10]  # [top, right, bottom, left] padding
-            ),
-            xaxis_opts=opts.AxisOpts(
-                name_location="middle",  # Place name at the end (bottom) of axis
-                name_gap=25,  # Gap between axis and name
-                axislabel_opts=opts.LabelOpts(
-                    margin=8  # Add margin between axis and labels
-                )
-            )
-        )
-
-        # Set grid options through chart options
-        chart.options["grid"] = {
-            "top": "20%",  # Chart area starts 20% from top
-            "bottom": "15%",  # Chart area ends 15% from bottom
-            "left": "10%",  # Chart area starts 10% from left
-            "right": "10%",  # Chart area ends 10% from right
-            "containLabel": True  # Include axis labels in the grid size calculation
-        }
-        
-        x_axis_column = self.options['x_axis_column']
-        y_axis_columns = self.options['y_axis_column']
-
-        # Get x-axis data
-        x_data = filtered_data[x_axis_column.field_name].tolist()
-        
-        # Add x-axis data
-        chart.add_xaxis(x_data)
-
-        # Add each line series
-        for y_axis_column in y_axis_columns:
-            series_name = y_axis_column.get('label') or y_axis_column['field'].field_name
-            field_name = y_axis_column['field'].field_name
-            value_mapping = y_axis_column.get('value_mapping', {})
-            
-            # Get y-values in the same order as x-values
-            y_values = []
-            for x in x_data:
-                row_data = filtered_data[filtered_data[x_axis_column.field_name] == x]
-                if not row_data.empty:
-                    val = row_data[field_name].iloc[0]
-                    y_values.append(0.0 if pd.isna(val) else float(val))
-                else:
-                    y_values.append(0.0)
-            
-            self.add_series_to_chart(
-                chart=chart,
-                series_name=series_name,
-                y_values=y_values,
-                color=y_axis_column.get('color'),
-                value_mapping=value_mapping
-            )
-
-        # Configure the chart
-        self.configure_chart(chart, filtered_data)
-        
-        return chart
