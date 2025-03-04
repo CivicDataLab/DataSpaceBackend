e86d2a86bbe257276e8ce78de5043c7d0f993631
dc <eopoxf@gmail.com>
2025-03-04 13:31:22 +0530

    remove search urls

--- DataSpace/urls.py
+++ DataSpace/urls.py
@@ -13,15 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import include, path, re_path
 
 from DataSpace import settings
 from search import urls as search_index_urls
 
 urlpatterns = [
     path("api/", include("api.urls")),
-    path('admin/', admin.site.urls),
-    re_path(r'^search/', include(search_index_urls)),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
