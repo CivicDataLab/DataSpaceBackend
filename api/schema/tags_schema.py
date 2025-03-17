ac7115010431ec64430202a60ec7d17682d4b279
dc <eopoxf@gmail.com>
2025-03-17 11:51:15 +0530

    add delete response type

--- api/schema/tags_schema.py
+++ api/schema/tags_schema.py
@@ -6,15 +6,23 @@ from api.models import Tag
 
 
 @strawberry.type
+class DeleteTagResponse:
+    """Response for delete_tag mutation."""
+
+    success: bool
+    message: str
+
+
+@strawberry.type
 class Mutation:
     """Mutations for tags."""
 
     @strawberry_django.mutation(handle_django_errors=True)
-    def delete_tag(self, info: Info, tag_id: str) -> bool:
+    def delete_tag(self, info: Info, tag_id: str) -> DeleteTagResponse:
         """Delete a tag."""
         try:
             tag = Tag.objects.get(id=tag_id)
         except Tag.DoesNotExist:
             raise ValueError(f"Tag with ID {tag_id} does not exist.")
         tag.delete()
-        return True
+        return DeleteTagResponse(success=True, message="Tag deleted successfully")
