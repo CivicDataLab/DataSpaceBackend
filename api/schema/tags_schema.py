e2a9e09ddf4b540f82aca66fb2df1f36ec8aaf5b
dc <eopoxf@gmail.com>
2025-03-31 19:12:18 +0530

    retrun bool instead of response for delete tag

--- api/schema/tags_schema.py
+++ api/schema/tags_schema.py
@@ -3,14 +3,7 @@ import strawberry_django
 from strawberry.types import Info
 
 from api.models import Tag
-
-
-@strawberry.type
-class DeleteTagResponse:
-    """Response for delete_tag mutation."""
-
-    success: bool
-    message: str
+from api.utils.graphql_telemetry import trace_resolver
 
 
 @strawberry.type
@@ -18,11 +11,14 @@ class Mutation:
     """Mutations for tags."""
 
     @strawberry_django.mutation(handle_django_errors=True)
-    def delete_tag(self, info: Info, tag_id: str) -> DeleteTagResponse:
+    @trace_resolver(
+        name="delete_tag", attributes={"component": "tag", "operation": "mutation"}
+    )
+    def delete_tag(self, info: Info, tag_id: str) -> bool:
         """Delete a tag."""
         try:
             tag = Tag.objects.get(id=tag_id)
         except Tag.DoesNotExist:
             raise ValueError(f"Tag with ID {tag_id} does not exist.")
         tag.delete()
-        return DeleteTagResponse(success=True, message="Tag deleted successfully")
+        return True
