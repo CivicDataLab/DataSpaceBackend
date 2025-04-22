bc22ed8a698f8866f7eb1e84d83e996edbcf15b2
dc <eopoxf@gmail.com>
2025-04-22 19:02:07 +0530

    update fields for org and dataset admin

--- api/admin.py
+++ api/admin.py
@@ -6,13 +6,13 @@ from api.models import Dataset, Organization
 # Register models needed for authorization app's autocomplete fields
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug", "created_at")
+    list_display = ("name", "slug", "created")
     search_fields = ("name", "slug")
     prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Dataset)
 class DatasetAdmin(admin.ModelAdmin):
-    list_display = ("title", "organization", "created_at")
+    list_display = ("title", "organization", "created")
     list_filter = ("organization",)
     search_fields = ("title", "description")
