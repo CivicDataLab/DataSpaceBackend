efe590b1ce2ab3598bd94310150504b88e9c6b60
dc <eopoxf@gmail.com>
2025-05-09 19:40:56 +0530

    add social medial details to organization

--- api/models/Organization.py
+++ api/models/Organization.py
@@ -29,6 +29,10 @@ class Organization(models.Model):
         related_name="parent_field",
     )
     slug = models.SlugField(max_length=75, null=True, blank=False, unique=True)
+    github_profile = models.URLField(blank=True, null=True)
+    linkedin_profile = models.URLField(blank=True, null=True)
+    twitter_profile = models.URLField(blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
 
     def save(self, *args: Any, **kwargs: Any) -> None:
         self.slug = slugify(self.name)
