f4ab1dd65fb0bef9e9564229088fa346d81b8276
dc <eopoxf@gmail.com>
2025-05-27 17:34:05 +0530

    move kc middleware to package

--- authorization/middleware/__init__.py
+++ authorization/middleware/__init__.py
@@ -1,1 +1,3 @@
-# This file is intentionally left empty to make the directory a Python package.
+# Import middleware classes to expose them at the package level
+from authorization.middleware.activity_consent import ActivityConsentMiddleware
+from authorization.middleware.keycloak_auth import KeycloakAuthenticationMiddleware
