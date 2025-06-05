821dfaaa02691f61340be69aaea0cacb59f129d8
dc <eopoxf@gmail.com>
2025-06-05 15:38:21 +0530

    Change hook for processing errors

--- api/utils/graphql_error_extension.py
+++ api/utils/graphql_error_extension.py
@@ -9,29 +9,36 @@ from api.utils.error_handlers import ErrorDictType, format_integrity_error
 
 
 class ErrorFormatterExtension(Extension):  # type: ignore[misc,valid-type]
-    def format_errors(self, errors: List[GraphQLError]) -> List[GraphQLError]:
-        return [self.format_error(error) for error in errors]
+    def process_errors(
+        self, errors: List[GraphQLError], execution_context: ExecutionContext
+    ) -> List[GraphQLError]:
+        formatted_errors = []
+        for error in errors:
+            original = getattr(error, "original_error", error)
 
-    def format_error(self, error: GraphQLError) -> GraphQLError:
-        original = getattr(error, "original_error", error)
-
-        if isinstance(original, IntegrityError):
-            error_data = format_integrity_error(original)
-            if "field_errors" in error_data:
-                field_errors = cast(Dict[str, List[str]], error_data["field_errors"])
-                # Return a properly formatted GraphQL error
-                return GraphQLError(
-                    message=next(iter(field_errors.values()))[0],
-                    path=error.path,
-                    extensions={"field_errors": field_errors},
-                )
+            if isinstance(original, IntegrityError):
+                error_data = format_integrity_error(original)
+                if "field_errors" in error_data:
+                    field_errors = cast(
+                        Dict[str, List[str]], error_data["field_errors"]
+                    )
+                    formatted_errors.append(
+                        GraphQLError(
+                            message=next(iter(field_errors.values()))[0],
+                            path=error.path,
+                            extensions={"field_errors": field_errors},
+                        )
+                    )
+                else:
+                    non_field_errors = cast(List[str], error_data["non_field_errors"])
+                    formatted_errors.append(
+                        GraphQLError(
+                            message=non_field_errors[0],
+                            path=error.path,
+                            extensions={"non_field_errors": non_field_errors},
+                        )
+                    )
             else:
-                non_field_errors = cast(List[str], error_data["non_field_errors"])
-                return GraphQLError(
-                    message=non_field_errors[0],
-                    path=error.path,
-                    extensions={"non_field_errors": non_field_errors},
-                )
+                formatted_errors.append(error)
 
-        # For other errors, return the original error
-        return error
+        return formatted_errors
