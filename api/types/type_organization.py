3e22e2c26479246588d909c8ce1757024f1f5dfd
dc <eopoxf@gmail.com>
2025-06-23 17:08:07 +0530

    return those usecases where org is owner as well

--- api/types/type_organization.py
+++ api/types/type_organization.py
@@ -2,6 +2,7 @@ from typing import Any, List, Optional
 
 import strawberry
 import strawberry_django
+from django.db.models import Q
 from strawberry import Info, auto
 
 from api.models import Organization
@@ -57,7 +58,7 @@ class TypeOrganization(BaseType):
                 return 0
 
             use_cases = UseCase.objects.filter(
-                usecaseorganizationrelationship__organization_id=org_id,  # type: ignore
+                (Q(organization__id=org_id) | Q(usecaseorganizationrelationship__organization_id=org_id)),  # type: ignore
                 status=UseCaseStatus.PUBLISHED.value,
             ).distinct()
             return use_cases.count()
