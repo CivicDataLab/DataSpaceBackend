34893bd634ad8678e26d6c519bfb7dc31204f54d
dc <eopoxf@gmail.com>
2026-02-11 14:01:20 +0530

    add slug to user info

--- api/views/auth.py
+++ api/views/auth.py
@@ -101,6 +101,7 @@ class UserInfoView(views.APIView):
                         "homepage": org.organization.homepage,  # type: ignore[attr-defined]
                         "created": org.organization.created,  # type: ignore[attr-defined]
                         "updated": org.organization.modified,  # type: ignore[attr-defined]
+                        "slug": org.organization.slug,  # type: ignore[attr-defined]
                     }
                     for org in user.organizationmembership_set.all()  # type: ignore[union-attr, arg-type]
                 ],
